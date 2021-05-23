e939cd37720c5c23801802ffeffaf360db65dc02
ergashboy1990 <ergashboyoblaqulov90@gmail.com>
2021-05-23 16:00:16 +0500

    create project

---
+++ src/main/java/com/springDars/SpringDarsApplication.java
@@ -0,0 +1,13 @@
+package com.springDars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springDars/SpringDarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springDars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
